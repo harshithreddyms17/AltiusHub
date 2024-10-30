a03417f9ebcb45e7f1f80ba571a3e309dc72ef21
harshithreddyms17 <97732233+harshithreddyms17@users.noreply.github.com>
2024-10-30 16:32:10 +0530

    Update calculateAmount.js

--- calculateAmount.js
+++ calculateAmount.js
@@ -1,3 +1,4 @@
+//validation
 function calculateAmount(items, billSundries) {
     const total = items.reduce((sum,item)=>sum+item.quantity*item.price,0);
     const billSundryT = billSundries.reduce((sum,bs)=>sum+bs.amount,0);
